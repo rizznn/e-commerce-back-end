8a9a500661ab994d9f52ea516a43a1e53ef89255
Riz <80712058+rizznn@users.noreply.github.com>
2021-09-12 22:01:52 -0400

    tags CRUD added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,6 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-
 });
 
 router.get('/:id', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No category found with this id' });
+        res.status(404).json({ message: 'No product found with this id' });
         return;
       }
       res.json(dbProductData);
@@ -143,7 +143,7 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({ message: 'No category found with this id' });
+        res.status(404).json({ message: 'No product found with this id' });
         return;
       }
       res.json(dbProductData);
